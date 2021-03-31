52cfb7d3cd72df66faacdce7968ab21ad5edc796
eeinfo <xunan321@hotmail.com>
2021-03-31 17:21:10 +0800

    Day8

---
+++ src/ch15/series/my_series.go
@@ -0,0 +1,22 @@
+package series
+
+import "fmt"
+
+func init() {
+	fmt.Println("init1")
+}
+func init() {
+	fmt.Println("init2")
+}
+
+func GetFibonacciSerie(n int) []int {
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList
+}
+
+func Square(n int) int {
+	return n * n
+}
